562d6ee63c2ed17bc277e7a53f6d7f4631b211ff
Axel Ledung <Axel.Ledung@quicksearch.se>
2024-12-06 15:36:30 +0100

    git inti

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HAF_Project.Models;
+
+namespace HAF_Project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //Insert models to create Tables
+        //Example:
+        //public DbSet<Languages> Languages { get; set; }
+    }
+}
